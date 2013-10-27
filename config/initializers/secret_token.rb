3be1b8567c7a72eb7d6e4f1f3ded7af24a12298f
Jennifer Hamon <jhamon@gmail.com>
2013-10-27 15:17:03 -0700

    Move snake js files into new rails project. Need rails for RESTful scoreboard API.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snakejs::Application.config.secret_key_base = '0898cf8373aaf3c4c25df907698fafcb147944b3f8f92d41a0de9cd8e240a3798f4fb3935d4ffbc2199280d29f74c8a4180352b80fe239ad0a0fac5237d1997a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snakejs::Application.config.session_store :cookie_store, key: '_snakejs_session'
