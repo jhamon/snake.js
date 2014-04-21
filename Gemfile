9231162d29831d6de10bdffee5bb4055d8293141
Jen Hamon <jhamon@gmail.com>
2014-04-21 03:25:21 -0700

    Add pagination to scores controller.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'backbone-on-rails'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jbuilder', '~> 1.2'
 gem 'jquery-rails'
+gem 'kaminari'
 gem 'rails', '4.0.0'
 gem 'sass-rails', '~> 4.0.0'
 gem 'turbolinks'
@@ -11,6 +12,9 @@ gem 'uglifier', '>= 1.3.0'
 
 group :development do
   gem 'sqlite3'
+  gem 'quiet_assets'
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
 group :production do
--- app/controllers/high_scores_controller.rb
+++ app/controllers/high_scores_controller.rb
@@ -1,14 +1,12 @@
 class HighScoresController < ApplicationController
-  respond_to :json
-
   def index
-    @high_scores = HighScore.order(score: :desc).limit(10)
-    respond_with(@high_scores)
+    @high_scores = HighScore.order(score: :desc).page(1)
+    render :json => @high_scores
   end
 
   def create
     @high_score = HighScore.new(high_score_params)
-    respond_with(@high_score)
+    render :json => @high_score
   end
 
   private
