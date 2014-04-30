b58402d65d35dcd81778de1590e2190c7680ad2b
Jen Hamon <jhamon@gmail.com>
2014-04-29 19:53:14 -0700

    Remove console.log statments.

--- app/assets/javascripts/routers/snakeRouter.js
+++ app/assets/javascripts/routers/snakeRouter.js
@@ -11,14 +11,12 @@ SnakeGame.Routers.snakeRouter = Backbone.Router.extend({
   startGame: function () {
     SnakeGame.gameState = new SnakeGame.Models.highScore();
     var gameView = new SnakeGame.Views.gameView();
-    console.log("Starting game.")
     this._swapView(gameView);
   },
 
   highScores: function () {
     var highScoresPage = new SnakeGame.Views.highScoresPage();
     this._swapView(highScoresPage);
-    console.log("Showing scores.")
   },
 
   _swapView: function (newView) {
